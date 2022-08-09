eb90b3ca01ec0535889e66daeb5ebe6f8152554b
Andro87 <ggandolfo987@gmail.com>
2022-08-09 17:21:23 +0200

    [Changes] -changed component

--- components/index.ts
+++ components/index.ts
@@ -2,7 +2,7 @@ export { Header } from "./header/Header";
 export { Btn } from "./btn_consultation/Btn";
 export { Main } from "./main/Main";
 export { Skill } from "./skill/Skill";
-export { MyWork } from "./my_work//Mywork";
+export { MyWork } from "./my_work/MyWork";
 export { Carousel } from "./carousel/Carousel";
 export { Book } from "./book/Book";
 export { Footer } from "./footer/Footer";
